8f34dc06babafba5cccd14a2c66a1abff32a924e
Kuroda Moeko <kuroda-moeko@outlook.jp>
2016-09-03 03:02:24 +0900

    目標④検索

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -63,7 +63,15 @@ class TasksController < ApplicationController
 
 def search 
  @search = params['search']['name'] 
+ @search_user = params['search']['user'] 
+ @search_category = params['search']['category'] 
  @tasks = Task.where("name like '%#{@search}%'") 
+ if @search_user.present? 
+   @tasks = @tasks.where(user_id: @search_user) 
+ end 
+ if @search_category.present? 
+   @tasks = @tasks.where(category_id: @search_category) 
+ end 
  render :index 
  end 
 
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   resources :tasks do   
     post 'search', on: :collection 
   end 
+  resources :users do 
+    post 'search', on: :collection 
+  end 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
